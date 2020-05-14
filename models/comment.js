28a73f1a64e08da2178e718a0b70897fdd462b05
J-Vincent <41374606+J-Vincent@users.noreply.github.com>
2020-05-14 00:49:58 -0400

    Delete some files.

--- models/comment.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
-var commentSchema = new mongoose.Schema({
-		user_name:String,
-		comment:String
-	});
-
-module.exports = mongoose.model("Comment",commentSchema);
